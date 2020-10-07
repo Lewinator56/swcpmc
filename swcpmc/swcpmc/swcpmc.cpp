dfd615c91c88a947cef259e77c2552c2be839322
lewis dixon <lewis.dixon@live.co.uk>
2020-10-07 20:03:49 +0100

    started work on the converting code, im ommiting anything not needed as there is a hell of a lot more to write to achieve the same stuff

--- swcpmc/swcpmc/swcpmc.cpp
+++ swcpmc/swcpmc/swcpmc.cpp
@@ -2,9 +2,15 @@
 //
 
 #include "swcpmc.h"
+#include <array>
+#include <sstream>
 
 using namespace std;
 using byte = unsigned char;
+string _pathToMesh;
+string _pathToOutput;
+byte bin[];
+
 
 class Triangle {
 public:
@@ -103,9 +109,126 @@ public:
 
 };
 
+class Color {
+public:
+	byte _r;
+	byte _g;
+	byte _b;
+	byte _a;
+	Color(byte r, byte g, byte b, byte a) {
+		_r = r;
+		_g = g;
+		_b = b;
+		_a = a;
+	}
+};
+
+//string split
+vector<string> split(string const& str, char const& delim) {
+	stringstream ss(str);
+	string s;
+	vector<string> out;
+	while (std::getline(ss, s, delim)) {
+		out.push_back(s);
+	}
+	return out;
+
+}
 
 
-int main()
+int main(int argc, char **argv)
 {
-	Vertex v = Vertex(10.0, 10.0, 10.0, 5, 5, 5, 5);
+	if (argc != 3) {
+		cout << "invalid number of arguements, please run this tool from the command line" << endl;
+		return 0;
+	}
+	else
+	{
+		_pathToMesh = argv[1];
+		_pathToOutput = argv[2];
+		cout << "Reading from " << _pathToMesh << ", Writing to " << _pathToOutput << endl;
+	}
+
+	// main bit of code
+	
+}
+
+
+
+void objToMesh(string data[]) {
+	string error = "";
+	try {
+		uint16_t vertexCount = 0;
+		Color c = Color(255, 125, 0, 255);
+		int vertexPosCount = 0;
+		vector<Normal> normals;
+		vector<int> subMeshVertices;
+		vector<SubMesh> submeshes;
+		int currentSubmesh = -1;
+		subMeshVertices.push_back(0);
+		bool needSubmesh = true;
+
+		for (int i = 0; i < sizeof(data); i++) {
+			try {
+				error = data[i].substr(0, data[i].find_first_of(' '));
+			}
+			catch (exception) {}
+
+			if (data[i].rfind('V', 0) == 0 && needSubmesh) {
+				SubMesh s;
+				s._r = c._r;
+				s._g = c._g;
+				s._b = c._b;
+				s._a = c._a;
+				currentSubmesh++;
+				submeshes.push_back(s);
+				needSubmesh = false;
+				
+
+			}
+
+			// object 'o'
+			if (data[i].rfind('o', 0) == 0) {
+				if (data[i].find('/') != string::npos) {
+					vector<string> out;
+					vector<string> cv = split(split(split(data[i], ' ')[1], '/')[0], '-');
+					c._r = stoi(cv[0]);
+					c._g = stoi(cv[1]);
+					c._b = stoi(cv[2]);
+					c._a = stoi(cv[3]);
+				}
+				else {
+					c._r = 255;
+					c._g = 125;
+					c._b = 0;
+					c._a = 255;
+				}
+
+				if (needSubmesh) {
+					SubMesh s;
+					s._r = c._r;
+					s._g = c._g;
+					s._b = c._b;
+					s._a = c._a;
+					currentSubmesh++;
+					submeshes.push_back(s);
+					needSubmesh = false;
+				}
+				else {
+					submeshes[currentSubmesh]._r = c._r;
+					submeshes[currentSubmesh]._g = c._g;
+					submeshes[currentSubmesh]._b = c._b;
+					submeshes[currentSubmesh]._a = c._a;
+				}
+			}
+
+			// vertex 'v'
+			else if (data[i].rfind('v', 0) == 0 && data[i].rfind("vt", 0) != 0 && data[i].rfind("vn", 0) != 0) {
+
+			}
+		}
+	}
+	catch (exception) {
+
+	}
 }
